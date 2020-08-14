9a70860091136f6535112a0c537272d74d534fc3
Arun <arun.sood1998@gmail.com>
2020-08-15 01:43:53 +0200

    new user authentication with JWT

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {faCoffee} from '@fortawesome/free-solid-svg-icons';
+import {AuthenticationService} from '../service/authentication.service';
 
 @Component({
   selector: 'app-header',
@@ -9,7 +10,7 @@ import {faCoffee} from '@fortawesome/free-solid-svg-icons';
 export class HeaderComponent implements OnInit {
   faCoffee = faCoffee;
 
-  constructor() {
+  constructor(public loginService: AuthenticationService) {
   }
 
   ngOnInit(): void {
